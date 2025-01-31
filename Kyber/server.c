8fcf78f63c63abf464a9132862fab8b7516c4798
floswrld <flvasica@icloud.com>
2025-01-31 22:48:45 +0100

    added debug info

--- Kyber/server.c
+++ Kyber/server.c
@@ -139,10 +139,8 @@ int main() {
         fprintf(stderr, "Failed to start HTTP server\n");
         return 1;
     }
-	printf("Server is running on the ip adress above ");
-    system("ifconfig | grep 'inet ' | grep -m 1 -Po '192\\.(?!255\\b)\\d{1,3}\\.(?!255\\b)\\d{1,3}\\.(?!255\\b)\\d{1,3}'");
-    printf("and Port %d\n", PORT);
 
+    printIpAddress();
      char input[128];
     while (1) {
         printf("Geben Sie 'stop' ein, um den Server zu beenden:\n");
