3e7a769852b22b6585df5dedc0e58ff3e90941f8
floswrld <flvasica@icloud.com>
2025-01-31 21:48:26 +0100

    Added debug

--- Kyber/server.c
+++ Kyber/server.c
@@ -28,8 +28,9 @@ static int request_handler(void *cls, struct MHD_Connection *connection,
                            size_t *upload_data_size, void **con_cls) {
     struct MHD_Response *response;
     int ret;
-
+	printf("Running check");
     if (strcmp(url, "/get_public_key") == 0 && strcmp(method, "GET") == 0) {
+      	printf("called get_public_key: %s\n", url);
         response = MHD_create_response_from_buffer(PQCLEAN_KYBER1024_CLEAN_CRYPTO_PUBLICKEYBYTES,
                                                    global_public_key, MHD_RESPMEM_PERSISTENT);
         ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
@@ -38,6 +39,7 @@ static int request_handler(void *cls, struct MHD_Connection *connection,
     }
 
     if (strcmp(url, "/send_encrypted_data") == 0 && strcmp(method, "POST") == 0) {
+      printf("called send_encrypted_data: %s\n", url);
         if (*upload_data_size > 0) {
             cJSON *json = cJSON_Parse(upload_data);
             if (!json) {
