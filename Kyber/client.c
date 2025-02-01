03898f0250c63b9a08813552bae635f7b8b04341
floswrld <flvasica@icloud.com>
2025-02-01 02:25:48 +0100

    Fixed /send_encrypted_data

--- Kyber/client.c
+++ Kyber/client.c
@@ -204,6 +204,7 @@ int main() {
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         return 1;
     }
+    printf("Heruntergeladene JSON-Daten:\n%s\n", chunk.memory);
     curl_easy_cleanup(curl_handle);
 
     FILE *csv_file = fopen(CSV_FILE, "w");
